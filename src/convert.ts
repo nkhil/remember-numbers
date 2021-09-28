a7a3dd189eb4ef807700dda50cfc987cc0b90fd7
Nikhil Vijayan <nikhil@tokencard.io>
2021-09-29 00:09:34 +0100

    Add tiny refactor

--- src/convert.ts
+++ src/convert.ts
@@ -1,10 +1,10 @@
-import pairs from './data/numbers'
+import numbers from './data/numbers'
 
 export function calculate(number: string): string {
   const chunked = number.match(/.{1,3}/g)
   if (!chunked) throw new Error('No pairs found')
   const resultArr = chunked.map(chunk => {
-    return pairs[chunk]
+    return numbers[chunk]
   })
   return resultArr.join('-')
 }
--- src/decode.ts
+++ src/decode.ts
@@ -1,9 +1,9 @@
-import pairs from './data/numbers'
+import numbers from './data/numbers'
 
 export function decode(string: string): string {
   const chunkArr = string.split('-')
   return chunkArr.reduce((acc, val) => {
-    const key = Object.keys(pairs).find(k => pairs[k] === val)
+    const key = Object.keys(numbers).find(k => numbers[k] === val)
     if (!key) throw new Error(`key ${key} not found!`)
     acc = acc.concat(key)
     return acc
